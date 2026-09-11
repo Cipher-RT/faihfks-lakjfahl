ed20312b778612c0a5b885845b7b155b812a3a59
mulka <munkhtulga.b2015@orkhonschool.edu.mn>
2026-09-11 15:40:43 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -8,8 +8,14 @@ def fibonacci(n):
         return fibonacci(n-1)+fibonacci(n-2)
 # Exercise 2
 def count_digits(n):
+    if n<10:
+        return 1
+    return count_digits(n//10)+1
     pass
 
 # Exercise 3
 def sum_digits(n):
+    if n < 10:
+        return n
+    return sum_digits(n // 10) + n%10
     pass
